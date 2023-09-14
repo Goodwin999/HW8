f6370ae18654ce5beb2f7d9528aca224306057a9
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2023-09-14 18:32:59 +0300

    Fix task 1/выполнил первую задачу

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home task!");
+
+        System.out.println("Task №1");
+        int [] time = new int []{1, 2, 3};
+        float [] month = new float[]{1.57f, 7.654f, 9.986f};
+        long [] year = new long[] {120_000L, 450L, 580_000L};
+
     }
 }
