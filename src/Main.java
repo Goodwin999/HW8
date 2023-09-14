1906714ffb653ebd9f6c3afd7eb38b44b244ec6c
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2023-09-14 22:07:52 +0300

    Fix task 2/выполнил 2 задачу

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home task!");
 
         System.out.println("Task №1");
-        int [] time = new int []{1, 2, 3};
-        float [] month = new float[]{1.57f, 7.654f, 9.986f};
-        long [] year = new long[] {120_000L, 450L, 580_000L};
+        int[] time = new int[]{1, 2, 3};
+        float[] month = new float[]{1.57f, 7.654f, 9.986f};
+        long[] year = {120_000L, 450L, 580_000L};
+
+
+        System.out.println("Task №2");
+        System.out.println(Arrays.toString(time));
+        System.out.println(Arrays.toString(month));
+        System.out.println(Arrays.toString(year));
+
+
+
 
     }
+
 }
+
+
